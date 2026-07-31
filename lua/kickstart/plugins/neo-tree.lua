dabb54109cc33c02aeafa008af3663baaf0ce8df
martinbuchta <tamabuch@gmail.com>
2026-07-31 10:31:12 +0200

    feat(neo-tree): adapt width and show all files

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -15,8 +15,28 @@ vim.pack.add(plugins)
 
 vim.keymap.set('n', '\\', '<Cmd>Neotree reveal<CR>', { desc = 'NeoTree reveal', silent = true })
 
+local function neo_tree_width()
+  -- Keep the tree compact on a laptop, but give long names more room in a
+  -- very wide terminal (the ultrawide setup is currently about 429 columns).
+  return vim.o.columns >= 380 and 60 or 40
+end
+
 require('neo-tree').setup {
+  window = {
+    width = neo_tree_width,
+  },
   filesystem = {
+    filtered_items = {
+      visible = true,
+      hide_dotfiles = false,
+      hide_gitignored = false,
+      hide_ignored = false,
+      hide_hidden = false,
+      hide_by_name = {},
+      hide_by_pattern = {},
+      never_show = {},
+      never_show_by_pattern = {},
+    },
     window = {
       mappings = {
         ['\\'] = 'close_window',
@@ -24,3 +44,17 @@ require('neo-tree').setup {
     },
   },
 }
+
+-- Reapply the appropriate width if the terminal moves between displays.
+vim.api.nvim_create_autocmd('VimResized', {
+  group = vim.api.nvim_create_augroup('neo-tree-responsive-width', { clear = true }),
+  callback = function()
+    local width = neo_tree_width()
+    for _, win in ipairs(vim.api.nvim_list_wins()) do
+      local buf = vim.api.nvim_win_get_buf(win)
+      if vim.bo[buf].filetype == 'neo-tree' then
+        vim.api.nvim_win_set_width(win, width)
+      end
+    end
+  end,
+})
